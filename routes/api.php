c98c3dada315f42b21fe8dbdc9386b3ddbe2eaaa
berkanergil <69623905+berkanergil@users.noreply.github.com>
2021-06-30 17:53:11 +0300

    last-commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
@@ -16,11 +16,26 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+//Public Routes
 
-Route::resource('products', ProductController::class);
-Route::get("/products/search/{name}",[
-    ProductController::class,"search"
-]);
+Route::post('/register', [AuthController::class,"register"]);
+Route::post('/login', [AuthController::class,"login"]);
+
+Route::get('/products', [ProductController::class,"index"]);
+Route::get('/products/{id}', [ProductController::class,"show"]);
+Route::get('/products/search/{name}', [ProductController::class,"search"]);
+
+
+//Protected Routes
+Route::group(["middleware"=>["auth:sanctum"]],function () {
+
+    Route::post('/logout', [AuthController::class,"logout"]);
+
+    Route::post('/products', [ProductController::class,"store"]);
+    Route::put('/products/{id}', [ProductController::class,"update"]);
+    Route::delete('/products/{id}', [ProductController::class,"destroy"]);
+
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
